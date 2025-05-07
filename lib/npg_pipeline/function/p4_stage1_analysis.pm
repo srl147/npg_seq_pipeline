c5b9b2e6170507ed69634536c3eec13ac54d017c
Your Name <srl@sanger.ac.uk>
2025-05-07 11:27:08 +0100

    bug fix

--- lib/npg_pipeline/function/p4_stage1_analysis.pm
+++ lib/npg_pipeline/function/p4_stage1_analysis.pm
@@ -588,7 +588,7 @@ sub _is_spladeseq {
   # I've restricted this to library_types which exactly match Duplex-Seq to exclude the old library_type Bidirectional Duplex-seq
   # the Duplex-Seq library prep has been replaced by the NanoSeq library prep, the analysis is the same and the Duplex-Seq library_type is still in use
   # so there is no need to rename this function but there are two new library_types Targeted NanoSeq Pulldown Twist and Targeted NanoSeq Pulldown Agilent
-  my $is_spladeseq = any {$_->library_type && $_->library_type =~/^SPLADE-seq/smx)}
+  my $is_spladeseq = any {$_->library_type && $_->library_type =~/^SPLADE-seq/smx}
                   $lane_lims->descendants();
 
   return $is_spladeseq;
